45c530a767cb2106d6fc484aad4e50e063db464b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-09 16:43:03 -0500

    feat: Add hsv module
    This module converts colors in HSV to RGB. This is required because
    Cairo only takes RGB colors, but we need HSV for this program.

---
+++ src/hsv.c
@@ -0,0 +1,49 @@
+void hsv_to_rgb(double h, double s, double v, double *r, double *g, double *b) {
+  if (s == 0) {
+    *r = *g = *b = v;
+  } else {
+    h *= 6;
+    if (h == 6) {
+      h = 0;
+    }
+
+    int i = (int)h;
+    double f = h - i;
+    double p = v * (1 - s);
+    double q = v * (1 - s * f);
+    double t = v * (1 - s * (1 - f));
+
+    switch (i) {
+    case 0:
+      *r = v;
+      *g = t;
+      *b = p;
+      break;
+    case 1:
+      *r = q;
+      *g = v;
+      *b = p;
+      break;
+    case 2:
+      *r = p;
+      *g = v;
+      *b = t;
+      break;
+    case 3:
+      *r = p;
+      *g = q;
+      *b = v;
+      break;
+    case 4:
+      *r = t;
+      *g = p;
+      *b = v;
+      break;
+    case 5:
+      *r = v;
+      *g = p;
+      *b = q;
+      break;
+    }
+  }
+}
---
+++ src/hsv.h
@@ -0,0 +1,6 @@
+#ifndef HSV_H
+#define HSV_H
+
+void hsv_to_rgb(double h, double s, double v, double *r, double *g, double *b);
+
+#endif
