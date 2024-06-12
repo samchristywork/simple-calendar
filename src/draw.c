252e90fc913db153940b0582e3df6dc6b9131d1b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-12 17:47:49 -0500

    feat: Add function to draw the current time
    This function draws a line at the current time of day on the column that
    represents the current day. Text that represents the current time of day
    in HH:MM format also appears under the line.

--- src/draw.c
+++ src/draw.c
@@ -33,3 +33,37 @@ void draw_modified_indicator(cairo_t *cr) {
     cairo_fill(cr);
   }
 }
+
+void draw_current_time(cairo_t *cr) {
+  float column_width = (float)(width - time_column_width) / 7;
+  float column_height = (float)height - header_height;
+
+  time_t current_time = time(NULL);
+  int day_start_time = get_start_of_week();
+  int current_day = (current_time - day_start_time) / (24 * 60 * 60);
+  int current_hour =
+      (current_time - day_start_time - current_day * 24 * 60 * 60) / (60 * 60);
+  int current_minute = (current_time - day_start_time -
+                        current_day * 24 * 60 * 60 - current_hour * 60 * 60) /
+                       60;
+
+  float x = time_column_width + current_day * column_width;
+  float y = header_height + current_hour * column_height / 24 +
+            current_minute * column_height / 24 / 60;
+
+  cairo_set_source_shade(cr, 0.0);
+  cairo_set_line_width(cr, 2.0);
+  cairo_move_to(cr, x, y);
+  cairo_line_to(cr, x + column_width, y);
+  cairo_stroke(cr);
+
+  char *time_str = malloc(6);
+  sprintf(time_str, "%02d:%02d", current_hour, current_minute);
+  cairo_text_extents_t extents;
+  cairo_text_extents(cr, time_str, &extents);
+
+  cairo_move_to(cr, x + column_width - extents.width - 10,
+                y + column_height / 24 / 2 - 2);
+  cairo_show_text(cr, time_str);
+  free(time_str);
+}
