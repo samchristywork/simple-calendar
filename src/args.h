8b22e4b5789416b826541c65ce97496a4f8fa822
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-15 12:35:05 -0500

    feat: Add args module
    This module enables me to handle command line arguments much more
    easily. Right now the only argument I'm interested in is the filename
    argument, but later I will have args for more things.

---
+++ src/args.h
@@ -0,0 +1,27 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+#include <stdbool.h>
+
+typedef enum FLAG {
+  ARG_NONE = 0,
+  ARG_REQUIRED = 1,
+  ARG_MULTIPLE = 2,
+} FLAG;
+
+typedef struct Arg {
+  const char *long_name;
+  const char *description;
+  FLAG flags;
+} Arg;
+
+void add_arg(char short_name, const char *long_name, const char *description,
+             FLAG flags);
+bool get_arg_bool(int argc, char *argv[], char short_name, bool default_value);
+int get_arg_int(int argc, char *argv[], char short_name, int default_value);
+char *get_arg_string(int argc, char *argv[], char short_name,
+                     char *default_value);
+char **get_arg_strings(int argc, char *argv[], char short_name, int *count);
+void usage(const char *program_name);
+
+#endif
